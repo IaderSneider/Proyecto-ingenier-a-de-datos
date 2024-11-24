01b5514db9ef93664db61d408e090723fd3345ca
Iader <iadersneider@gmail.com>
2024-11-24 14:09:01 -0500

    Se crearon los datos para la pagina web y se hicieron pruebas

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -1,22 +1,39 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc
+import dash
+from dash import dcc, html
 import pandas as pd
 import plotly.express as px
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-# Initialize the app
-app = Dash()
+df_subida = pd.read_csv('Pagina_web/banda_subida.csv')
+df_bajada=pd.read_csv('Pagina_web/banda_bajada.csv')
 
-# App layout
-app.layout = [
-    html.Div(children='My First App with Data and a Graph'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
-]
+# Crear gráfico de líneas con Plotly Express
+fig_banda_subida = px.line(
+    df_subida,
+    x='MUNICIPIO',  # El eje X muestra los nombres
+    y='ANCHO DE BANDA DE SUBIDA (MB)',   # El eje Y muestra los valores
+    title="Ancho de banda subida",
+    labels={"nombre": "Nombre", "valor": "Valor"}
+)
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+fig_banda_bajada = px.line(
+    df_bajada,
+    x='MUNICIPIO',  # El eje X muestra los nombres
+    y='ANCHO DE BANDA DESCARGA (MB)',   # El eje Y muestra los valores
+    title="Ancho de banda bajada",
+    labels={"nombre": "Nombre", "valor": "Valor"}
+)
 
+# Inicializar Dash
+app = dash.Dash(__name__)
+
+# Layout de la aplicación
+app.layout = html.Div([
+    html.H1("Gráfico de Líneas Interactivo", style={"textAlign": "center"}),
+    dcc.Graph(id="line-chart",figure=fig_banda_subida),
+    dcc.Graph(id="line-chart",figure=fig_banda_bajada)
+])
+
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    app.run_server(debug=True)
