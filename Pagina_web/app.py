f911859213a7aec9cb8f260f8611e55b7cd6db12
Iader <iadersneider@gmail.com>
2024-11-25 00:14:56 -0500

    se creo la pagina web

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -6,34 +6,58 @@ import plotly.express as px
 
 df_subida = pd.read_csv('Pagina_web/banda_subida.csv')
 df_bajada=pd.read_csv('Pagina_web/banda_bajada.csv')
+df_n_sedes_operador= pd.read_csv('Pagina_web/n_sedes_operador.csv')
+df_n_sedes_operador_estado=pd.read_csv('Pagina_web/n_sedes_operador_limpio.csv')
 
-# Crear gráfico de líneas con Plotly Express
+
+df_subida['promedio total']=df_subida['promedio'].mean()
+df_bajada['promedio total']=df_bajada['promedio'].mean()
+
 fig_banda_subida = px.line(
     df_subida,
-    x='MUNICIPIO',  # El eje X muestra los nombres
-    y='ANCHO DE BANDA DE SUBIDA (MB)',   # El eje Y muestra los valores
+    x='municipio',
+    y=['promedio', 'promedio total'],  
     title="Ancho de banda subida",
-    labels={"nombre": "Nombre", "valor": "Valor"}
 )
+
 
 fig_banda_bajada = px.line(
     df_bajada,
-    x='MUNICIPIO',  # El eje X muestra los nombres
-    y='ANCHO DE BANDA DESCARGA (MB)',   # El eje Y muestra los valores
+    x='municipio',  
+    y=['promedio', 'promedio total'], 
     title="Ancho de banda bajada",
-    labels={"nombre": "Nombre", "valor": "Valor"}
+
 )
 
-# Inicializar Dash
+fig_n_sedes_operador= px.bar(
+    df_n_sedes_operador,
+    x="operador",  # Eje X con los diferentes operadores
+    y="numero de sedes",  # Eje Y con la cantidad total de cada grupo
+    color="operador",  # Color por el estado
+    title="Distribución de Count por Operador y Estado",
+    barmode="stack",  # apilar las barras para mostrar la distribución por estado
+)
+
+fig = px.sunburst(
+    df_n_sedes_operador_estado,
+    path=["operador", "estado"],  # Estructura jerárquica
+    values="count",  # Tamaño de los sectores
+    color="count",  # Color basado en la población
+    color_continuous_scale="Viridis",  # Escala de colores
+    title="Distribución jerárquica de población por región"
+)
+
 app = dash.Dash(__name__)
 
-# Layout de la aplicación
+
 app.layout = html.Div([
     html.H1("Gráfico de Líneas Interactivo", style={"textAlign": "center"}),
     dcc.Graph(id="line-chart",figure=fig_banda_subida),
-    dcc.Graph(id="line-chart",figure=fig_banda_bajada)
+    dcc.Graph(id="line-chart",figure=fig_banda_bajada),
+    dcc.Graph(id="sunburst-chart",figure=fig_n_sedes_operador),
+    dcc.Graph(id="bar-chart",figure=fig)
 ])
 
-# Ejecutar la aplicación
+
 if __name__ == "__main__":
     app.run_server(debug=True)
