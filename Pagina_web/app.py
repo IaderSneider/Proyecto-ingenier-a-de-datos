82bbeb77f50961bad51fb53620679322c7505a4d
Iader <iadersneider@gmail.com>
2024-11-26 22:43:44 -0500

    pagina web

--- Pagina_web/app.py
+++ Pagina_web/app.py
@@ -3,6 +3,7 @@ from dash import dcc, html
 import pandas as pd
 import plotly.express as px
 
+color_fondo='#a6acaf'
 
 df_subida = pd.read_csv('Pagina_web/banda_subida.csv')
 df_bajada=pd.read_csv('Pagina_web/banda_bajada.csv')
@@ -16,8 +17,27 @@ df_bajada['promedio total']=df_bajada['promedio'].mean()
 fig_banda_subida = px.line(
     df_subida,
     x='municipio',
-    y=['promedio', 'promedio total'],  
-    title="Ancho de banda subida",
+    y=['promedio', 'promedio total'], 
+    color_discrete_map={
+        "promedio": "#f4d03f ",
+        "promedio total": "white",
+    }
+    
+)
+
+fig_banda_subida.update_layout(
+    paper_bgcolor=color_fondo,  
+    plot_bgcolor=color_fondo,   
+    font=dict(color="white"),  
+    legend=dict(font=dict(color="white")),  
+    xaxis=dict(
+        gridcolor="#424949",  # Cuadrícula en gris claro
+        zerolinecolor="white",  # Línea del cero en azul oscuro
+    ),
+    yaxis=dict(
+        gridcolor="#424949",  # Cuadrícula en gris claro
+        zerolinecolor="white",  # Línea del cero en azul oscuro
+    )
 )
 
 
@@ -25,39 +45,128 @@ fig_banda_bajada = px.line(
     df_bajada,
     x='municipio',  
     y=['promedio', 'promedio total'], 
-    title="Ancho de banda bajada",
+    color_discrete_map={
+        "promedio": "#f4d03f ",
+        "promedio total": "white",
+    }
+)
 
+fig_banda_bajada.update_layout(
+    paper_bgcolor=color_fondo,  
+    plot_bgcolor=color_fondo,   
+    font=dict(color="white"),  
+    legend=dict(font=dict(color="white")),  
+    xaxis=dict(
+        gridcolor="#424949",  # Cuadrícula en gris claro
+        zerolinecolor="white",  # Línea del cero en azul oscuro
+    ),
+    yaxis=dict(
+        gridcolor="#424949",  # Cuadrícula en gris claro
+        zerolinecolor="white",  # Línea del cero en azul oscuro
+    )
 )
 
 fig_n_sedes_operador= px.bar(
     df_n_sedes_operador,
-    x="operador",  # Eje X con los diferentes operadores
-    y="numero de sedes",  # Eje Y con la cantidad total de cada grupo
-    color="operador",  # Color por el estado
-    title="Distribución de Count por Operador y Estado",
-    barmode="stack",  # apilar las barras para mostrar la distribución por estado
+    x="operador", 
+    y="numero de sedes",  
+    color="operador",  
+    
+    barmode="stack", 
+)
+
+fig_n_sedes_operador.update_layout(
+    paper_bgcolor=color_fondo,  
+    plot_bgcolor=color_fondo,   
+    font=dict(color="white"),  
+    legend=dict(font=dict(color="white")),  
 )
 
 fig = px.sunburst(
     df_n_sedes_operador_estado,
-    path=["operador", "estado"],  # Estructura jerárquica
-    values="count",  # Tamaño de los sectores
-    color="count",  # Color basado en la población
-    color_continuous_scale="Viridis",  # Escala de colores
-    title="Distribución jerárquica de población por región"
+    path=["sede","operador", "estado"],  
+    values="count",  
+    color="count",  
+    color_continuous_scale="Viridis",  
+    
 )
+fig.update_layout(
+    paper_bgcolor=color_fondo,  
+    plot_bgcolor=color_fondo,   
+    font=dict(color="white"),  
+    legend=dict(font=dict(color="white")),  
+)
+
+analisis_subida=dcc.Markdown('''
+
+    **Ventajas:**
+    - Facilidad para detectar patrones y áreas con baja capacidad.
+    - Visualiza claramente cómo varía el ancho de banda en los municipios.
+
+    **Desventajas:**
+    - Puede volverse saturado y difícil de leer con mucha información.'''
+    )
+
+analisis_bajada=dcc.Markdown(
+    '''
+    **Ventajas:**
+    - Claridad para observar la tendencia del ancho de banda de bajada.
+    - Permite detectar áreas con potenciales problemas de conectividad.
+    - Facilita la comparación entre diferentes municipios.
+
+    **Desventajas:**
+    - No es adecuado para comparar demasiados municipios.
+    - Los valores exactos no son fácilmente legibles sin interacción adicional.
+    '''
+    )
+
+analisis_sedes= dcc.Markdown(
+    '''
+    **Ventajas:**
+    - Permite una comparación clara entre operadores.
+    - Fácil de leer y entender de manera visual.
+    - Proporciona una visión rápida de la distribución de sedes por operador.
+
+    **Desventajas:**
+    - Se vuelve menos efectivo si se tiene un número muy alto de operadores.
+    - Puede resultar menos útil si hay muchos operadores en la misma categoría.
+    '''
+    )
+
+analisis_sedes_operador_estado= dcc.Markdown(
+    '''
+    **Ventajas:**
+    - Excelente para representar jerárquicamente la distribución de sedes.
+    - Facilita la interacción y exploración de datos a diferentes niveles.
+    - Visualiza la estructura de datos de manera intuitiva.
+
+    **Desventajas:**
+    - Puede ser difícil de leer si hay demasiados niveles de información.
+    - Puede resultar confuso para algunos usuarios no familiarizados con este tipo de visualización.
+    - Puede volverse saturado y difícil de leer con mucha información.
+    '''
+    )
+
 
 app = dash.Dash(__name__)
 
 
+
 app.layout = html.Div([
-    html.H1("Gráfico de Líneas Interactivo", style={"textAlign": "center"}),
-    dcc.Graph(id="line-chart",figure=fig_banda_subida),
+    html.H1("Instituciones esducativas con conexion a internet"),
+    html.H2('Promedio ancho de banda de subida por cada municipio'),
+    dcc.Graph(id="line-chart1",figure=fig_banda_subida),
+    analisis_subida,
+    html.H2('Promedio ancho de banda de bajada por cada municipio'),
     dcc.Graph(id="line-chart",figure=fig_banda_bajada),
+    analisis_bajada ,
+    html.H2('Número de sedes por cada operador'),
     dcc.Graph(id="sunburst-chart",figure=fig_n_sedes_operador),
-    dcc.Graph(id="bar-chart",figure=fig)
+    analisis_sedes,
+    html.H2('Número de sedes agrupadas por operador y estado'),
+    dcc.Graph(id="bar-chart",figure=fig),
+    analisis_sedes_operador_estado
 ])
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+app.run_server(debug=True)
--- README.md
+++ README.md
@@ -9,4 +9,38 @@
 - [Departamentos](https://www.dian.gov.co/atencionciudadano/formulariosinstructivos/Formularios/2012/departamentos_2012.pdf)   
 [Aechivo](Tablas/departamento.csv)
 
+# Apreciaciones del Proceso de Desarrollo del Proyecto
+
+El desarrollo del proyecto de implicó varias etapas. A continuación, presento mis apreciaciones respecto a cada aspecto clave del proceso:
+
+### Selección de Fuentes de Datos
+- **Desafíos:**  
+  En algunos casos, la consolidación y limpieza de los datos (por ejemplo, manejo de formatos, valores nulos o duplicados) puede haberse convertido en un paso laborioso. 
+
+### Diseño de la Base de Datos
+- **Aciertos:**  
+  La elección de **PostgreSQL** como sistema gestor de base de datos fue adecuada, dada su capacidad para manejar datos estructurados y consultas complejas. El diseño probablemente permitió una estructura clara, con tablas bien normalizadas para facilitar consultas y evitar redundancias.
+- **Desafíos:**  
+  Una etapa crítica fue modelar adecuadamente las relaciones entre entidades como municipios, instituciones educativas y operadores. Asegurarse de incluir índices y claves primarias/foráneas bien definidas.
+
+
+### Carga de Datos
+
+- **Desafíos:**  
+  Validar la calidad de los datos durante la carga (como formatos de fechas, latitudes y longitudes) es siempre una tarea que requiere atención especial.
+
+## Conexión con la Base de Datos
+- **Aciertos:**  
+  Usar bibliotecas como **psycopg2** para conectar PostgreSQL con python facilitó las consultas y la integración de datos.
+
+### Desarrollo Usando Dash
+- **Aciertos:**  
+  Dash demostró ser una excelente elección para el desarrollo de las visualizaciones interactivas y la interfaz del proyecto.
+- **Desafíos:**  
+  Diseñar un layout atractivo y funcional en Dash puede ser complejo, especialmente si se desea un diseño altamente personalizado.
+
+# Conclusión
+El proyecto destaca por su capacidad para generar percepciones útiles sobre la conectividad en instituciones educativas, con aplicaciones prácticas tanto para la planificación pública como para los operadores de servicios de internet. Las herramientas seleccionadas, combinadas con un enfoque sistemático, demuestran un manejo sólido de tecnologías modernas y metodologías de desarrollo de software. Una mayor automatización y mejoras en el diseño visual serían los próximos pasos ideales para escalar el impacto del proyecto.
+
+
 
